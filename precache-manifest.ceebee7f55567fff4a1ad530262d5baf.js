14fd0422a77ccb1a21f1dbbce43b9c4f1e769c93
OSUser <dshwarcman@gmail.com>
2019-07-29 16:15:34 +0300

    Updates

---
+++ precache-manifest.ceebee7f55567fff4a1ad530262d5baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2149b0c7e41c1ad34392f7fe3f459b2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "918aa27463dc37454fde",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "df8fc2fb7c7effb1c600",
+    "url": "/static/js/2.5a08db1e.chunk.js"
+  },
+  {
+    "revision": "918aa27463dc37454fde",
+    "url": "/static/js/main.d19d7e5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a396670f9169263baeb071880a740f3",
+    "url": "/static/media/profile.8a396670.jpg"
+  }
+]);
