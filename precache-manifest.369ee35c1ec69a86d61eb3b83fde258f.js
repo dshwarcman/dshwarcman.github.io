935db342eeb953b236591af7b37d0c623afc406f
dshwarcman <dshwarcman@gmail.com>
2019-07-30 16:20:39 +0300

    Updates

---
+++ precache-manifest.369ee35c1ec69a86d61eb3b83fde258f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2da02dea7a905dc336656da9554e126",
+    "url": "/jerdash-portfolio/index.html"
+  },
+  {
+    "revision": "c08eeb3cfc6280c33d1e",
+    "url": "/jerdash-portfolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "df8fc2fb7c7effb1c600",
+    "url": "/jerdash-portfolio/static/js/2.5a08db1e.chunk.js"
+  },
+  {
+    "revision": "c08eeb3cfc6280c33d1e",
+    "url": "/jerdash-portfolio/static/js/main.6c3057db.chunk.js"
+  },
+  {
+    "revision": "ccf1aa4ebb874ac13917",
+    "url": "/jerdash-portfolio/static/js/runtime~main.c7eddda6.js"
+  },
+  {
+    "revision": "8a396670f9169263baeb071880a740f3",
+    "url": "/jerdash-portfolio/static/media/profile.8a396670.jpg"
+  }
+]);
